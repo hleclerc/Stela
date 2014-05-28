dccd123e2a438f203587a159c94cebe6db19c7f4
Hugo LECLERC <leclerc@lmt.ens-cachan.fr>
2014-05-28 16:56:50 +0200

    missing files

---
+++ rew/src/Stela/Inst/DeclBaseClass.h
@@ -0,0 +1,15 @@
+DECL_BT( Type ) // MUST BE the first item
+
+DECL_BT( Error )
+DECL_BT( Void )
+
+DECL_BT( SurdefList )
+DECL_BT( Class )
+DECL_BT( Def )
+
+DECL_BT( SourceFile )
+
+DECL_BT( VarargsItemEnd )
+
+#include "DeclArytTypes.h"
+
---
+++ rew/src/Stela/Ir/AssignFlags.h
@@ -0,0 +1,11 @@
+#ifndef Stela_Ir_AssignFlags_H
+#define Stela_Ir_AssignFlags_H
+
+enum AssignFlags {
+    IR_ASSIGN_REF = 1,
+    IR_ASSIGN_STATIC = 2,
+    IR_ASSIGN_CONST = 4,
+    IR_ASSIGN_TYPE = 8
+};
+
+#endif // Stela_Ir_AssignFlags_H
